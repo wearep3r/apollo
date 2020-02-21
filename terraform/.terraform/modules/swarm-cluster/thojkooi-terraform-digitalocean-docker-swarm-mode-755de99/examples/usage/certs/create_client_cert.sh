3acf537d6c9b582b277bd3c7bf9fea8f10cafaaa
Fabian Peter <fabian@peter.saarland>
2020-02-21 08:23:24 +0100

    fix: removed legacy code

--- terraform/.terraform/modules/swarm-cluster/thojkooi-terraform-digitalocean-docker-swarm-mode-755de99/examples/usage/certs/create_client_cert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export ADDRESS=
-export NAME=$1
-echo '{"CN":"'$NAME'","hosts":[""],"key":{"algo":"rsa","size":4096}}' | cfssl gencert -config=ca-config.json -ca=ca.pem -ca-key=ca-key.pem -hostname="$ADDRESS" - | cfssljson -bare $NAME
