9e8a70489763fa8bff2023f8fe1fa495660b1496
sventropy <sven@hennessen.net>
2020-04-22 22:10:17 +0200

    fix: allow http(s) traffic to ec2 instance

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -18,6 +18,18 @@ resource "aws_security_group" "sg" {
     to_port     = 22
     protocol    = "tcp"
   }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+  }
   // Terraform removes the default rule
   egress {
     from_port   = 0
