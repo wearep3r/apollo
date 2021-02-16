9cbe219181017d5e0e4cb936d006be1c1350868f
Fabian Peter <fabian@peter.saarland>
2021-02-16 23:29:07 +0100

    feat: implemented CNAB

---
+++ helpers.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+install() {
+  echo "ansible-playbook install.yml $@"
+  chmod 0700 /root/.ssh/id_rsa
+  ansible-playbook install.yml "$@"
+}
+
+upgrade() {
+  echo "ansible-playbook upgrade.yml $@"
+  chmod 0700 /root/.ssh/id_rsa
+  ansible-playbook upgrade.yml "$@"
+}
+
+uninstall() {
+  echo "ansible-playbook uninstall.yml $@"
+  chmod 0700 /root/.ssh/id_rsa
+  ansible-playbook uninstall.yml "$@"
+}
+
+# Call the requested function and pass the arguments as-is
+"$@"
